ff2fb7f895a1d3900be9122b673b5a0076445a7b
CodeWithBigboss-source <malikahsannasar@gmail.com>
2026-05-03 00:39:42 +0500

    analysis.py

--- analysis.py
+++ analysis.py
@@ -2,6 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 data = pd.read_csv("matches.csv")
+deliveries = pd.read_csv("deliveries.csv")
+
+
+
 
 print(data.head())
 print(data.columns)
@@ -41,3 +45,36 @@ plt.bar(top_players.index, top_players.values)
 plt.title("Top Players (MOTM Awards)")
 plt.xticks(rotation=45)
 plt.show()
+
+
+print(deliveries.head())
+
+top_batsmen = deliveries.groupby("batter")["batsman_runs"].sum().sort_values(ascending=False)
+print(top_batsmen.head(10))
+
+sixes = deliveries[deliveries["batsman_runs"] == 6]
+top_six_hitters = sixes["batter"].value_counts().head(5)
+print(top_six_hitters)
+
+fours = deliveries[deliveries["batsman_runs"] == 4]
+top_four_hitters = fours["batter"].value_counts().head(5)
+print(top_four_hitters)
+
+# import matplotlib.pyplot as plt
+
+top10 = top_batsmen.head(10)
+
+plt.bar(top10.index, top10.values)
+plt.xticks(rotation=45)
+plt.title("Top 10 Batsmen by Runs")
+plt.show()
+
+merged = deliveries.merge(data, left_on="match_id", right_on="id")
+winning_runs = merged[merged["winner"] == merged["team1"]]
+
+top_winning_batsmen = winning_runs.groupby("batter")["batsman_runs"].sum().sort_values(ascending=False)
+
+print(top_winning_batsmen.head(10))
+
+impact_players = merged["player_of_match"].value_counts().head(10)
+print(impact_players)
